15bc6bf124ae9cefdb77353637a6c5cb8ee8cb07
EC2 Default User <ec2-user@ip-172-31-46-76.us-east-2.compute.internal>
2022-03-13 02:10:44 +0000

    added server file

---
+++ server.py
@@ -0,0 +1,19 @@
+import socket
+
+server = socket.socket()
+
+server.bind( ( socket.gethostname() , 6969) )
+server.listen()
+
+input("PRESS ENTER")
+
+while True:
+    clientsocket, address = server.accept()
+    print( f"connection from {address} has been made" )
+    
+    clientsocket.send(bytes("hello!", "utf-8") )
+
+
+def send_data( address ):
+    clientsocket.send
+
